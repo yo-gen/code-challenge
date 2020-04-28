e0769e0c157ce05d48a741d0444ce13f83c37794
Manuel Ortega <maoc84@gmail.com>
2020-04-28 16:55:22 +0200

    Adds Bootstrap dependencies (jquery popper.js)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,7 @@ require("channels");
 require("trix");
 require("@rails/actiontext");
 
+import "bootstrap";
 import "css/site";
 
 // Uncomment to copy all static images under ../images to the output folder and reference
