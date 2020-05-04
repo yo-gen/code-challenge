d22b10ca7734fc228165ce32eba4b12215dc529b
Hassan <hassanakbar2@gmail.com>
2020-05-04 19:58:50 +0500

    email domain validation added on company

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,7 @@
 class Company < ApplicationRecord
   has_rich_text :description
 
+  EMAIL_REGEX = /\A[a-zA-Z0-9_.+-]+@(?:(?:[a-zA-Z0-9-]+\.)?[a-zA-Z]+\.)?(getmainstreet)\.com\z/
 
+  validates :email, allow_blank: true, format: { with: EMAIL_REGEX, message: I18n.t('company.invalid_email') }
 end
