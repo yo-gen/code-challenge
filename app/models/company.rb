bedc3aee063c7c1d44b888d7327aabb0f46761bb
Amit Agarwal <amit.agarwal@weinvest.net>
2020-06-14 18:17:26 +0530

    add city and state attribute code

--- app/models/company.rb
+++ app/models/company.rb
@@ -6,13 +6,25 @@ class Company < ApplicationRecord
   has_rich_text :description
 
   validate :email_check
+  after_save_commit :set_city_and_state
+
+  after_initialize :set_city_and_state
 
   private
 
   def email_check
     if (CUSTOM_EMAIL_ALLOWED) and (email.split("@").last != "getmainstreet.com")
+ #TODO: We can i18n for message
       errors.add(:email, 'Sorry! Only getmainstreet.com users are allowed to register at this time')
     end unless email.blank?
   end
+
+  def set_city_and_state
+	zipcode_details = ZipCodes.identify(zip_code)
+    unless zipcode_details.nil?
+	    self.city = zipcode_details[:city]
+	    self.state = zipcode_details[:state_code]
+		end
+	end
 
 end
