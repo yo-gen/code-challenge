da1d5eca4ee22311ab216e768bdfeed4ebf63991
Amit Agarwal <amit.agarwal@weinvest.net>
2020-06-14 17:22:25 +0530

    added migration for two new fields state and city

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_14_110431) do
+ActiveRecord::Schema.define(version: 2020_06_14_115053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,8 @@ ActiveRecord::Schema.define(version: 2020_06_14_110431) do
     t.string "zip_code"
     t.string "phone"
     t.string "email"
+    t.string "city"
+    t.string "state"
   end
 
   create_table "versions", force: :cascade do |t|
