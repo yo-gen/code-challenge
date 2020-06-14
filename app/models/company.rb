7e5a3a2e6c39e6f8166a92e7a125044c5a0e481f
Amit Agarwal <amit.agarwal@weinvest.net>
2020-06-14 17:17:30 +0530

    email validation changes

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,18 @@
 class Company < ApplicationRecord
+
+	CUSTOM_EMAIL_ALLOWED= true
+
 	has_paper_trail
   has_rich_text :description
 
+  validate :email_check
+
+  private
+
+  def email_check
+    if (CUSTOM_EMAIL_ALLOWED) and (email.split("@").last != "getmainstreet.com")
+      errors.add(:email, 'Sorry! Only getmainstreet.com users are allowed to register at this time')
+    end unless email.blank?
+  end
 
 end
--- test/controllers/companies_controller_test.rb
+++ test/controllers/companies_controller_test.rb
@@ -47,7 +47,7 @@ class CompaniesControllerTest < ApplicationSystemTestCase
       fill_in("company_name", with: "New Test Company")
       fill_in("company_zip_code", with: "28173")
       fill_in("company_phone", with: "5553335555")
-      fill_in("company_email", with: "new_test_company@test.com")
+      fill_in("company_email", with: "new_test_company@getmainstreet.com")
       click_button "Create Company"
     end
 
@@ -63,5 +63,19 @@ class CompaniesControllerTest < ApplicationSystemTestCase
     assert_equal "New Test Company", last_company.name
     assert_equal "28173", last_company.zip_code
   end
+
+test "Valid Email for Company" do
+  visit new_company_path
+
+  within("form#new_company") do
+    fill_in("company_name", with: "wrong Email Company")
+    fill_in("company_zip_code", with: "28173")
+    fill_in("company_phone", with: "5553335555")
+    fill_in("company_email", with: "new_test_company@getmainstreet.com")
+    assert_no_difference('Company.count') do
+      click_button 'Create Company'
+    end
+  end
+end
 
 end
