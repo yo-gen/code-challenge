c4b8dde3702b979f9e44d5b3e6a476b099b1b355
John Salzarulo <johnsalzarulo@gmail.com>
2020-10-20 14:03:25 -0700

    Added JavaScript examples

--- app/javascript/controllers/example_controller.js
+++ app/javascript/controllers/example_controller.js
@@ -1,24 +1,45 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = ["button", "textToReverse"];
+  static targets = ["button", "textToReverse", "inputWarning"];
 
   initialize() {
-    console.log("Example Controller");
+    console.log("Example Controller initialize()");
+  }
+
+  entry(e) {
+    if (this.textToReverseTarget.value.length !== 0) {
+      hideButtons(this.buttonTargets);
+      this.inputWarningTarget.classList.add("d-none");
+    } else {
+      showButtons(this.buttonTargets);
+      this.inputWarningTarget.classList.remove("d-none");
+    }
   }
 
   reverse(e) {
-    console.log("button clicked!");
-    this.textToReverseTarget.value = processString(
+    this.textToReverseTarget.value = reverseString(
       this.textToReverseTarget.value
     );
   }
+
+  uppercase(e) {
+    this.textToReverseTarget.value = this.textToReverseTarget.value.toUpperCase();
+  }
 }
 
-function processString(str) {
+function reverseString(str) {
   return str.split("").reverse().join("");
 }
 
-function toggleButtonDisabledState(btn) {
-  btn.disabled.toggle;
+function hideButtons(btns) {
+  btns.forEach(function (element) {
+    element.classList.remove("d-none");
+  });
+}
+
+function showButtons(btns) {
+  btns.forEach(function (element) {
+    element.classList.add("d-none");
+  });
 }
